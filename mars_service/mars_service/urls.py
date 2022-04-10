234d381a7b96b5af6dd27b03803b300c7ba36de5
dodir <mokronosovmw@gmail.com>
2022-04-10 16:42:23 +0500

    'better'

--- mars_service/mars_service/urls.py
+++ mars_service/mars_service/urls.py
@@ -20,6 +20,8 @@ from django.urls import include, path
 from django.conf.urls.static import static
 import orders_app.views
 
+# Comment
+
 urlpatterns = [
     path('orders_app/', include('orders_app.urls')),
     path('admin/', admin.site.urls),
