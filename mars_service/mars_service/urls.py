c503ed27e4bcd08db8f52a96b26a4faff006f85c
dodir <mokronosovmw@gmail.com>
2022-04-10 16:25:04 +0500

    [upd] better

--- mars_service/mars_service/urls.py
+++ mars_service/mars_service/urls.py
@@ -18,12 +18,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
-import orders_app.views
+from orders_app import views
 
 urlpatterns = [
-    path('orders_app/', include('orders_app.urls')),
-    path('admin/', admin.site.urls),
-    path('', orders_app.views.mainpage, name='mainpage'),
-    path('devices/', orders_app.views.get_devices, name='get_devices'),
-    path('devpage/', orders_app.views.devpage, name='devpage')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('orders_app/', include('orders_app.urls')),
+                  path('admin/', admin.site.urls),
+                  path('', views.mainpage, name='mainpage'),
+                  path('devices/', views.get_devices, name='get_devices'),
+                  path('devpage/', views.devpage, name='devpage')
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
