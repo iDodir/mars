ca89291d1641bd71d5fd297689a5348bcf8b0b04
dodir <mokronosovmw@gmail.com>
2022-04-10 16:51:51 +0500

    [upd] better

--- mars_service/mars_service/urls.py
+++ mars_service/mars_service/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# Yet another comment
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
