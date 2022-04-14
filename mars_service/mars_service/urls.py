7b816d028c5cd31f67d31015f7b792f3dcf66339
dodir <mokronosovmw@gmail.com>
2022-04-14 22:03:49 +0500

    This is my excellent single commit

--- mars_service/mars_service/urls.py
+++ mars_service/mars_service/urls.py
@@ -18,15 +18,17 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
-from orders_app import views
+import orders_app.views
 
 # Comment
 # Another comment to peter branch
+# One more comment to peter branch
+# Last comment to peter branch
 
 urlpatterns = [
                   path('orders_app/', include('orders_app.urls')),
                   path('admin/', admin.site.urls),
-                  path('', views.mainpage, name='mainpage'),
-                  path('devices/', views.get_devices, name='get_devices'),
-                  path('devpage/', views.devpage, name='devpage')
+                  path('', orders_app.views.mainpage, name='mainpage'),
+                  path('devices/', orders_app.views.get_devices, name='get_devices'),
+                  path('devpage/', orders_app.views.devpage, name='devpage')
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
